2416526ec6788ba8ad81763ac797f57e3b2c66b5
Rubhi Aulia Tirta <rubhiauliatirtacom@gmail.com>
2021-06-06 14:33:41 +0700

    add cors

--- server/models/room.js
+++ server/models/room.js
@@ -1,34 +1,34 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
 
-  const {Model } = sequelize.Sequelize
-  class Room extends Model{}
+  const { Model } = sequelize.Sequelize
+  class Room extends Model { }
 
   Room.init({
     name: {
-      type : DataTypes.STRING,
-      validate : {
+      type: DataTypes.STRING,
+      validate: {
         len: {
-          args: [4,12],
+          args: [4, 12],
           msg: "String length is not in this range"
-        } 
+        }
       }
     },
     players: {
-      type : DataTypes.JSON,
-      validate : {
-        isLessThanFour(value){
-          if(Object.keys(value).length > 4) {
+      type: DataTypes.JSON,
+      validate: {
+        isLessThanFour(value) {
+          if (Object.keys(value).length > 4) {
             throw new Error('Maximum player is 4')
           }
         }
       }
-    } 
+    }
   }, {
     sequelize
   })
 
-  Room.associate = function(models) {
+  Room.associate = function (models) {
     // associations can be defined here
   };
   return Room;
--- server/server.js
+++ server/server.js
@@ -1,42 +1,42 @@
 const http = require("./http")
-const io = require("socket.io")(http)
+const io = require("socket.io")(http, { origins: '*:*' })
 const RoomController = require("./controllers/room")
 require("./config/cron")
 
-io.on("connection", function(socket){
+io.on("connection", function (socket) {
 
   //===== BAGIAN LIST ROOM ======
-  socket.on('create-room', function(roomData){
+  socket.on('create-room', function (roomData) {
 
-    RoomController.create(roomData, function(err, createdRoom){
-      if(err) {
+    RoomController.create(roomData, function (err, createdRoom) {
+      if (err) {
         socket.emit('show-error', 'Failed to create room')
       } else {
         io.emit('room-created', createdRoom) //send room to all client
         socket.join(createdRoom.name) //daftarin creator room ke room yang dia bikin
-        socket.emit('get-in-to-room', {...createdRoom, playerKey: `1-${roomData.creator}`, isCreator : true}) //trigger creator untuk masuuk ker roomnya
+        socket.emit('get-in-to-room', { ...createdRoom, playerKey: `1-${roomData.creator}`, isCreator: true }) //trigger creator untuk masuuk ker roomnya
       }
-      
+
     })
   })
 
-  socket.on("join-room", function(payload){
-    RoomController.join(payload, function(err, result){
-      if (err){
-        socket.emit('show-error', 'Failed to join '+ payload.roomName )
-      } else  {
+  socket.on("join-room", function (payload) {
+    RoomController.join(payload, function (err, result) {
+      if (err) {
+        socket.emit('show-error', 'Failed to join ' + payload.roomName)
+      } else {
         socket.join(payload.roomName) //daftarin player ke room yang dia mau join
         io.to(payload.roomName).emit('player-joined', result.players) // kabarin ke anggota room lain kalo ada yang join
         socket.emit('get-in-to-room', result) //nyuruh yang join untuk masuk ke room
         io.emit('update-client-room') //trigger semua client agar update rooms nya
       }
-       
+
     })
   })
 
-  socket.on('get-rooms', function(){
-    RoomController.findAll(function(err, results){
-      if(err){
+  socket.on('get-rooms', function () {
+    RoomController.findAll(function (err, results) {
+      if (err) {
         socket.emit('show-error', "Failed to get room data")
       } else {
         socket.emit("get-rooms", results) //trigger client untuk menampilkan rooms
@@ -46,23 +46,23 @@ io.on("connection", function(socket){
 
   // ====== BAGIAN PLAY ROOM ========
 
-  socket.on('leave-room', function(payload){
+  socket.on('leave-room', function (payload) {
     socket.leave(payload.roomName)
-    RoomController.leave(payload, function(err, result){
+    RoomController.leave(payload, function (err, result) {
       io.to(payload.roomName).emit('player-left', result.players) // kabarin ke anggota room lain kalo ada yang leave
       io.emit('update-client-room')
     })
   })
 
-  socket.on('change-isplaying', function(payload){
+  socket.on('change-isplaying', function (payload) {
     io.to(payload.roomName).emit('change-isplaying', payload.isPlaying) //trigger room untuk memulai/menghentikan permainan
   })
-  socket.on('update-score', function(payload){
+  socket.on('update-score', function (payload) {
     socket.broadcast.to(payload.roomName).emit('update-score', payload) //trigger client untuk mengupdate score
   })
-  socket.on('end-game', function(roomName){
-    RoomController.delete(roomName, function(err){
-      if(!err){
+  socket.on('end-game', function (roomName) {
+    RoomController.delete(roomName, function (err) {
+      if (!err) {
         io.emit('update-client-room') //trigger semua client agar update rooms nya
       }
     })
